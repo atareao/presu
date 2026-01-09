5a36b19d3ec1a5b12fd2d7a0dc64e84a741b31cb
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2026-01-09 07:54:10 +0100

    ✨ Actualiza estructura de la tabla unidades con nuevos campos y restricciones

--- backend/migrations/20251201194335_units.up.sql
+++ backend/migrations/20251201194335_units.up.sql
@@ -1,10 +1,10 @@
 -- Units of Measure Table
 CREATE TABLE units (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(10) NOT NULL UNIQUE,
-    symbol VARCHAR(4) NOT NULL,
-    description VARCHAR(50),
-    formula TEXT NOT NULL, 
+    unit VARCHAR(30) NOT NULL UNIQUE,  -- Metro | Metro cuadrado
+    symbol VARCHAR(4) NOT NULL UNIQUE, -- m     | m2
+    formula TEXT NOT NULL,             -- a     | a * b
+    params JSONB NOT NULL,             -- ["a"] | ["a", "b"]
     -- Audit Fields
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC')
